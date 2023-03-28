5e52400077477b97dc72d6c5a9ea589e118bc126
Thomas Nguyen <captaintn3612@gmail.com>
2023-03-28 20:03:01 +0000

    I imported the univoltine_butterfly.csv data

---
+++ Scripts/01_import_unvoltine_butterfly_data.R
@@ -0,0 +1,12 @@
+#___________________________----
+# SET UP ----
+## An analysis of the correlation between temperature and size in the Silver Spotted Skipper, Hesperia comma ----
+
+## Here, I import the univoltine_butterfly.csv data
+butterfly <- read_csv("Data/univoltine_butterfly.csv")
+
+
+
+
+
+
