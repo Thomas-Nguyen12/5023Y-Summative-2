34c3f51ea529f11048d4757bb75cbc2d12b8b50b
Thomas Nguyen <captaintn3612@gmail.com>
2023-04-12 10:47:48 +0000

    I performed exploratory data analysis on the univoltine_butterfly dataset

---
+++ Scripts/03_exploratory_analysis.R
@@ -0,0 +1,95 @@
+## Linear Model
+
+library(tidyverse)
+library(ggplot2)
+library(ggpubr)
+
+## Here, i plot forewing_length against year 
+butterfly %>% 
+  ggplot(aes(year, forewing_length)) + geom_point() + 
+  geom_smooth(method="lm", se=FALSE) + 
+  stat_cor(method="pearson", p.accuracy=0.001, r.accuracy=0.01)
+
+
+## Here, i plot forewing_length against year for males and females
+butterfly %>% 
+  ggplot(aes(year, forewing_length)) + geom_point(aes(colour=sex)) + 
+  geom_smooth(aes(colour=sex), method="lm", se=FALSE) + 
+  stat_cor(method="pearson", p.accuracy=0.001, r.accuracy=0.01, aes(colour=sex))
+## plot shows no relationship between year and forewing_length
+
+
+
+
+## Here, I plot june_mean_rain against year for males and females
+butterfly %>% ggplot(aes(year, june_mean_rain)) + geom_point(aes(colour=sex)) + 
+  geom_smooth(aes(colour=sex), method="lm", se=FALSE) + 
+  stat_cor(method="pearson", p.accuracy=0.001, r.accuracy=0.01, aes(colour=sex))
+
+
+
+## Compare male and female forewing_length
+butterfly %>% ggplot(aes(sex, forewing_length)) + geom_jitter(aes(colour=sex)) + 
+  stat_summary(fun.y=mean, geom="crossbar", width=0.2) + 
+  geom_segment(aes(x=1, xend=2, y=mean(female_butterfly$forewing_length), yend=mean(male_butterfly$forewing_length)), linetype="dashed") + 
+  stat_compare_means(paired=FALSE, method="t.test", label.x.npc="right", comparisons=list(c("female", "male")), aes(label = sprintf("p = %5.4f", as.numeric(..p.format..))))
+
+
+stat.test <- shapiro.test(
+  butterfly$forewing_length
+  
+)
+butterfly %>% ggplot(aes(forewing_length)) + geom_density(aes(colour=sex, fill=sex)) + stat_pvalue_manual(stat.test, label="p.adj", y.position=0.8, x.position=12, aes(colour=sex))
+## Results show that forewing_length is statistically different between males and females
+## The plot also shows that both male and femlae forewing_length are normally distributed
+
+
+
+
+
+
+
+
+
+## Here, I plot the relationship between june_mean_rain and forewing_length between males and females
+butterfly %>% ggplot(aes(june_mean_rain, forewing_length)) + geom_point(aes(colour=sex)) + 
+  geom_smooth(se=FALSE, aes(colour=sex)) + 
+  stat_cor(method="spearman", aes(colour=sex), label.x=c(400, 400), p.accuracy=0.001, r.accuracy=0.001)
+## Results show no correlation between june_mean_rain and forewing_length
+
+
+
+
+## Here, I plot the relationship between june_mean_temperature and forewing_length for all butterflies
+
+butterfly %>% ggplot(aes(june_mean_temperature, forewing_length)) + geom_point() + 
+  geom_smooth(method="lm", se=FALSE) + 
+  stat_cor(method="pearson", p.accuarcy=0.001, r.accuracy=0.01)
+
+# This plot suggests that there is a very weak positive correlation between june_mean_temperature and the forewing_length of all butterflies
+
+
+
+
+
+## Here, I check the performance of this linear model
+performance::check_model(butterfly_lsmodel, check=c("normality", "qq", "homogeneity", "residuals"))
+
+
+
+## Here, I compare the correlation between june_mean_temperature and forewing_length for males and females
+
+butterfly %>% ggplot(aes(june_mean_temperature, forewing_length)) + 
+  geom_point(aes(colour=sex)) + geom_smooth(method="lm", se=TRUE, aes(colour=sex)) + 
+  stat_cor(method="pearson", aes(colour=sex), p.accuracy=0.001) + 
+  stat_regline_equation(label.y=c(15.3, 14.6), aes(colour=sex))
+## This plot suggests that there is a weak positive correlation between female forewing_length and june_mean_temperature
+## However, there is a moderate positive correlation between male forewing_length and june_mean_temperature
+
+
+
+
+
+
+
+
