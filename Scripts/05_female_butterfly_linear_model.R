2026b59ec0d5d80524197422ace411a61f7675cb
Thomas Nguyen <captaintn3612@gmail.com>
2023-04-15 11:56:08 +0000

    I created a Linear model relating female butterfly forewing_length with june_mean_temperature - accounting for outliers.

---
+++ Scripts/05_female_butterfly_linear_model.R
@@ -0,0 +1,70 @@
+## FEMALE LINEAR MODEL
+
+library(ggplot2)
+library(ggpubr)
+library(ISLR)
+
+
+## Creating a female subset of butterfly dataset
+female_butterfly <- butterfly %>% filter(sex == "female")
+
+##__________________________----
+## PLOTS ----
+
+## plotting forewing_length and june_mean_temperature in female butterflies
+female_butterfly %>% ggplot(aes(june_mean_temperature, forewing_length)) + geom_point() + geom_smooth(method="lm", se=TRUE) + stat_cor(method="pearson", label.y=15) + stat_regline_equation(geom="text", label.y=14.9)
+
+## Checking format of female data subset
+str(female_butterfly)
+
+
+##__________________________----
+## MODEL ----
+
+
+
+## Creating a linear model for female butterfly forewing length and june_mean_temperature
+## R-squared value indicates no relationship
+female_lsmodel <- lm(forewing_length ~ june_mean_temperature, data=female_butterfly)
+summary(female_lsmodel)
+
+## Checking model performance
+## boxcox suggests highly likely linear relationship
+performance::check_model(female_lsmodel)
+MASS::boxcox(female_lsmodel)
+
+## finding outliers
+## found outliers at june_mean_temperature = 20, 26 and 30
+cooksD <- cooks.distance(female_lsmodel)
+influential <- cooksD[(cooksD > (3 * mean(cooksD, na.rm = TRUE)))]
+influential
+
+## Creating a new model excluding outliers
+## Model showed slight improvements
+## model still exhibits weak positive correlation
+
+female_lsmodel2 <- lm(forewing_length[-30:-20] ~ june_mean_temperature[-30:-20], data=female_butterfly)
+summary(female_lsmodel2)
+
+## Checking performance of female_lsmodel2
+performance::check_model(female_lsmodel2)
+MASS::boxcox(female_lsmodel2)
+
+## Female lsmodel2 is preferred
+
+##__________________________----
+## PLOTS ----
+
+## Plotting female_lsmodel2
+forewing_length <- female_butterfly$forewing_length[-30:-20]
+june_mean_temperature <- female_butterfly$june_mean_temperature[-30:-20]
+
+female_lsmodel2_tibble <- tibble(june_mean_temperature, forewing_length)
+
+female_lsmodel2_tibble %>% ggplot(aes(june_mean_temperature, forewing_length)) + geom_point() + geom_smooth(method="lm") + stat_cor(method="pearson", p.accuracy=0.001, r.accuracy=0.001) + stat_regline_equation(label.y=14.8)
+
+
+
+
+
+
